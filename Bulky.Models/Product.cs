b95237df6d60b45298972bd0b6160e31036ab8d9
Rashed Bahnasi <rashedbh@hr.rikaz.tech>
2024-11-14 20:27:32 +0300

    fix image

--- Bulky.Models/Product.cs
+++ Bulky.Models/Product.cs
@@ -58,6 +58,8 @@ namespace BulkyBook.Models
         [ValidateNever]
         public Category Category { get; set; }
         [ValidateNever]
+        public string? ImageUrl { get; set; }
+        [ValidateNever]
         public List<ProductImage> ProductImages { get; set; }
     }
 }
